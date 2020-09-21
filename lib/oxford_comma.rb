80d443c498dc42cc0f8ffc93a6a54e4303773647
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-09-21 00:32:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def assign_rooms(array)
   
   
   array.with_index do |element, index|
-    "Hello, #{element}! You'll be assigned to room #{counter}!"
-    counter += 1 
+    "Hello, #{element}! You'll be assigned to room #{index + 1}!"
+     
   end
    
 end
